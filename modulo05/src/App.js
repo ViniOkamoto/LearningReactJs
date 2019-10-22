22bceafc2e1dc7e31778db74204db39d8c586c36
Vinicius Okamoto <53801461+ViniOkamoto@users.noreply.github.com>
2019-10-21 21:25:08 -0300

    commit Rotas, Estilização escopada e global

--- modulo05/src/App.js
+++ modulo05/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import Routes from './routes';
+import GlobalSytle from './styles/global';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <>
+      <Routes />
+      <GlobalSytle />
+    </>
   );
 }
 
---
+++ modulo05/src/pages/Main/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Title } from './styles';
+
+function Main() {
+  return <Title>Olá Mundo;</Title>;
+}
+
+export default Main;
